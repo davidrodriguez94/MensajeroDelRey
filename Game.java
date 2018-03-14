8b52f59d18ed94910ce1c6fbeebd6e948279cd2c
davidrodriguez94 <rodriguezdiezdavid94@gmail.com>
2018-03-14 11:12:32 +0100

    commit 10. Metodo look añadido que nos permite saber en que habitacion estamos

--- Game.java
+++ Game.java
@@ -129,6 +129,9 @@ public class Game
         else if (commandWord.equals("quit")) {
             wantToQuit = quit(command);
         }
+        else if (commandWord.equals("look")) {
+            look();
+        }
 
         return wantToQuit;
     }
@@ -146,7 +149,7 @@ public class Game
         System.out.println("as fast as u can.");
         System.out.println();
         System.out.println("Your command words are:");
-        System.out.println("   go quit help");
+        System.out.println("   go quit help look");
     }
 
     /** 
@@ -198,6 +201,14 @@ public class Game
         System.out.println(currentRoom.getLongDescription());
         
         System.out.println();
+    }
+
+    /**
+     * Print out the info about the current room
+     */
+    public void look()
+    {
+        System.out.println(currentRoom.getLongDescription());
     }
 
 }
